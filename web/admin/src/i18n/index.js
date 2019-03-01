250e00d799edcaad13b8a2db60ddb780ddb99dba
Adam Liechty <adam@doubledutch.me>
2019-02-28 16:08:10 -0800

    translations for arabic, german, spanish, french, italian, japanese, dutch

--- web/admin/src/i18n/index.js
+++ web/admin/src/i18n/index.js
@@ -1,11 +1,30 @@
 import enUS from './en-us/safeapp.admin.json'
-// import deDE from './de-de/safeapp.admin.json'
+
+import arINT from './ar-int/safeapp.admin.json'
+import deDE from './de-de/safeapp.admin.json'
+import esES from './es-es/safeapp.admin.json'
+import frFR from './fr-fr/safeapp.admin.json'
+import itIT from './it-it/safeapp.admin.json'
+import jaJP from './ja-jp/safeapp.admin.json'
+import nlNL from './nl-nl/safeapp.admin.json'
 
 export default {
   '': enUS,
   en: enUS,
   'en-US': enUS,
 
-  // "de":    deDE,
-  // "de-DE": deDE,
+  ar: arINT,
+  'ar-INT': arINT,
+  de: deDE,
+  'de-DE': deDE,
+  es: esES,
+  'es-ES': esES,
+  fr: frFR,
+  'fr-FR': frFR,
+  it: itIT,
+  'it-IT': itIT,
+  ja: jaJP,
+  'ja-JP': jaJP,
+  nl: nlNL,
+  'nl-NL': nlNL,
 }
